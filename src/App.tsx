9391171c5c0333e712653e943831050a64e09dc7
Anyeling0620 <3463645195@qq.com>
2025-12-04 23:21:53 +0800

    修复了商品详情页出错问题（合并时闭包错误）

--- src/App.tsx
+++ src/App.tsx
@@ -11,10 +11,10 @@ import FlashSalePage from "./pages/FlashSalePage"
 import ProductDetail from "./pages/ProductDetail"
 import ShoppingCart from "./pages/ShoppingCart"
 import Checkout from "./pages/Checkout"
+// 1. 确保引入了 CartProvider
 import { CartProvider } from "./context/CartContext"
 import UserCenter from "./pages/UserCenter"
 import UserLayout from "./component/Layout/UserLayout"
-
 
 /**
  * App组件：应用程序的主要组件，负责路由配置和布局
@@ -22,31 +22,38 @@ import UserLayout from "./component/Layout/UserLayout"
  */
 function App() {
   return (
+    
+      <main>
+        <CartProvider> 
+        <Toaster position="top-center" reverseOrder={false}
+          toastOptions={{
+            style: {}
+          }} />
+        
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route path="/" element={<Index />} />
+            <Route path="index" element={<Index />} />
+            <Route path='login' element={<Login />} />
+            <Route path='register' element={<Register />} />
+            <Route path='new-product' element={<NewProduct />} />
+            <Route path="search" element={<Search />} />
+            <Route path="flash-sale" element={<FlashSalePage />} />
+            
+              <Route path="product/:id" element={<ProductDetail />} />
+              <Route path="shopping-cart" element={<ShoppingCart />} />
+              <Route path="checkout" element={<Checkout />} />
+          </Route>
+          
+          <Route element={<UserLayout />}>
+            <Route path="user-center" element={<UserCenter />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        </CartProvider>
 
-    <main>
-      <Toaster position="top-center" reverseOrder={false}
-        toastOptions={{
-          style: {}
-        }} />
-      <Routes>
-        <Route element={<MainLayout />}>
-          <Route path="/" element={<Index />} />
-          <Route path="index" element={<Index />} />
-          <Route path='login' element={<Login />} />
-          <Route path='register' element={<Register />} />
-          <Route path='new-product' element={<NewProduct />} />
-          <Route path="search" element={<Search />} />
-          <Route path="flash-sale" element={<FlashSalePage />} />
-          <Route path="product/:id" element={<ProductDetail />} />
-          <Route path="shopping-cart" element={<ShoppingCart />} />
-          <Route path="checkout" element={<Checkout />} />
-        </Route>
-        <Route element={<UserLayout />}>
-          <Route path="user-center" element={<UserCenter />} />
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </main>
+      </main>
+    
   )
 }
 
