0ab7e401b69b2e7c269f5e926a120c218d2646a9
不见霞 <15550238+yvi-ksm@user.noreply.gitee.com>
2026-01-11 11:04:37 +0800

    '1.11'

--- src/utils/imageConfig.ts
+++ src/utils/imageConfig.ts
@@ -4,7 +4,7 @@ export const IMAGE_CONFIG = {
     FOLDERS: {
         ROLL: import.meta.env.VITE_ROLL_FOLDER,
         USER_AVATAR: import.meta.env.VITE_USER_AVATAR_FOLDER || 'static/images/user/avatar', // 用户头像
-        IMAGES: import.meta.env.VITE_IMAGES_FOLDER,
+        IMAGES: import.meta.env.VITE_IMAGES_FOLDER || 'static/images/lenovo',
     }
 } as const;
 
